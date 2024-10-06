afd044191739960121becc6c5da9ab5d91f36981
DGutierrezLeal <a01568890@tec.mx>
2024-10-05 21:45:44 -0600

    SSL vite certs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
     port: 4000, // Desired port
     host: "0.0.0.0", // Allows access from the local network
     strictPort: true, // Fail if the port is already in use
+    https: true
   },
-  plugins: [react()],
+  plugins: [react(), mkcert()],
 });
